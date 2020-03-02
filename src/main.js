f0c2df4adfca15214b163e48711cec6bb3fee2fc
Picknickguitar <nicguitar@web.de>
2020-03-03 00:17:05 +0100

    Test Design 1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMaterial from 'vue-material'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,43 @@
 <template>
   <div class="home">
-    
+    <div class="padding">
+      <md-toolbar>
+        <h3 class="md-title" style="flex: 1">Title</h3>
+        <md-button class="md-icon-button">
+          <md-icon>more_vert</md-icon>
+        </md-button>
+      </md-toolbar>
+
+      <md-toolbar class="md-accent" md-elevation="1">
+        <h3 class="md-title" style="flex: 1">Title</h3>
+        <md-button>Refresh</md-button>
+        <md-button class="md-primary">Create</md-button>
+      </md-toolbar>
+
+      <md-toolbar class="md-large md-primary">
+        <div class="md-toolbar-row">
+          <div class="md-toolbar-section-start">
+            <md-button class="md-icon-button">
+              <md-icon>menu</md-icon>
+            </md-button>
+          </div>
+
+          <div class="md-toolbar-section-end">
+            <md-button class="md-icon-button">
+              <md-icon>refresh</md-icon>
+            </md-button>
+
+            <md-button class="md-icon-button">
+              <md-icon>more_vert</md-icon>
+            </md-button>
+          </div>
+        </div>
+
+        <div class="md-toolbar-row md-toolbar-offset">
+          <h3 class="md-title">Title on a second row</h3>
+        </div>
+      </md-toolbar>
+    </div>
   </div>
 </template>
 
@@ -15,3 +52,20 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .md-toolbar {
+    margin-top: 16px;
+    
+  }
+
+  .md-title {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .padding {
+    padding: 0 20px;
+  }
+
+</style>
